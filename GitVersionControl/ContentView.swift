db1007d98cadc41a7b10e64327d76c8a5a3adad5
TreeShaker <dszeng219@hotmail.com>
2024-03-19 15:32:47 +0100

    Initial Commit

---
+++ GitVersionControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitVersionControl
+//
+//  Created by 曾嘉骏 on 2024/3/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitVersionControl/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  GitVersionControl
+//
+//  Created by 曾嘉骏 on 2024/3/19.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
